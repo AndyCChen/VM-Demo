ce909fddbda322cfaf09d9ad6cb150e52b92e7ac
AndyCChen <acchen@cpp.edu>
2024-11-22 13:16:06 -0800

    save work

--- src/components/VmSim.tsx
+++ src/components/VmSim.tsx
@@ -1,11 +1,22 @@
 import Grid from '@mui/material/Grid2'
+import Stack from '@mui/material/Stack'
 import React, { useState } from 'react'
+import { CONFIG } from '../utils/config'
 
-const TLB_SIZE = 3
-
+// tlb entries
 type TLB = {
    VPN: number | null,
    PPN: number | null,
+}
+
+// page table entries
+type PTB = {
+   PPN: number | null,
+   presentBit: boolean,
+}
+
+type PhysicalMem = {
+   PPN: number,
 }
 
 export default function VmSim({ virtualAddress }: { virtualAddress: number }) {
@@ -14,10 +25,31 @@ export default function VmSim({ virtualAddress }: { virtualAddress: number }) {
 
    const [tlbEntries, setTlbEntries] = useState<TLB[]>(() => {
       const entries: TLB[] = []
-      for (let i = 0; i < TLB_SIZE; ++i) {
+      for (let i = 0; i < CONFIG.TLB_SIZE; ++i) {
          entries.push({
             VPN: null,
             PPN: null
+         })
+      }
+      return entries
+   })
+
+   const [pageTable, setPageTable] = useState<PTB[]>(() => {
+      const entries: PTB[] = []
+      for (let i = 0; i < CONFIG.PAGE_TBL_SIZE(); ++i) {
+         entries.push({
+            PPN: null,
+            presentBit: false
+         })
+      }
+      return entries
+   })
+
+   const [physicalMemory, setPhysicalMemory] = useState<PhysicalMem[]>(() => {
+      const entries: PhysicalMem[] = []
+      for (let i = 0; i < CONFIG.PHYSICAL_PAGES; ++i) {
+         entries.push({
+            PPN: i,
          })
       }
       return entries
@@ -27,17 +59,17 @@ export default function VmSim({ virtualAddress }: { virtualAddress: number }) {
       <>
          <h2>Virtual Memory</h2>
          <Grid id='Virtual Address bits' container spacing={0}>
-            <Grid size={6} sx={{ border: '1px solid grey' }}>
+            <Grid size={4} sx={{ border: '1px solid grey' }}>
                Page Number
             </Grid>
-            <Grid size={6} sx={{ border: '1px solid grey' }}>
+            <Grid size={8} sx={{ border: '1px solid grey' }}>
                Page Offset
             </Grid>
 
-            <Grid size={6} sx={{ border: '1px solid grey' }}>
+            <Grid size={4} sx={{ border: '1px solid grey' }}>
                {virtualAddress === -1 ? '2-bits' : virtualPageNumber}
             </Grid>
-            <Grid size={6} sx={{ border: '1px solid grey' }}>
+            <Grid size={8} sx={{ border: '1px solid grey' }}>
                {virtualAddress === -1 ? '4-bits' : virtualPageOffset}
             </Grid>
          </Grid>
@@ -63,7 +95,39 @@ export default function VmSim({ virtualAddress }: { virtualAddress: number }) {
             }
          </Grid>
 
-         
+         <Grid container spacing={10} id='PageTablePhysicalMemContainer'>
+            <Grid size={7} id='PageTable'>
+               <h4>Page Table</h4>
+               <Grid container>
+                  <Grid sx={{ border: '1px solid grey' }} size={2}>Index</Grid>
+                  <Grid sx={{ border: '1px solid grey' }} size={3}>Present</Grid>
+                  <Grid sx={{ border: '1px solid grey' }} size={7}>PPN</Grid>
+                  {
+                     pageTable.map((ptb_entry, index) =>
+                        <React.Fragment key={index}>
+                           <Grid size={2} sx={{ border: '1px solid grey' }}>{index}</Grid>
+                           <Grid size={3} sx={{ border: '1px solid grey' }}>{ptb_entry.presentBit ? 1 : 0}</Grid>
+                           <Grid size={7} sx={{ border: '1px solid grey' }}>{ptb_entry.PPN === null ? '-' : ptb_entry.PPN}</Grid>
+                        </React.Fragment>
+                     )
+                  }
+               </Grid>
+            </Grid>
+
+            <Grid size={5} id='PhysicalMemory'>
+               <h4>Physical Memory</h4>
+               <Grid container>
+                  <Grid size={12} sx={{ border: '1px solid grey' }}>Physical Pages</Grid>
+                  {
+                     physicalMemory.map((entry, index) =>
+                        <React.Fragment key={index}>
+                           <Grid size={12} sx={{ border: '1px solid grey' }}>{entry.PPN}</Grid>
+                        </React.Fragment>
+                     )
+                  }
+               </Grid>
+            </Grid>
+         </Grid>
 
 
 
--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,4 +1,11 @@
 export const CONFIG = Object.freeze({
 	MAX: 64,
-   
+	TLB_SIZE: 3,
+	PHYSICAL_PAGES: 3,
+   PAGE_TBL_SIZE: function () {
+		return ((this.MAX - 1) >>> 4) + 1
+	},
+	PAGE_SIZE: function () {
+		return ((this.MAX - 1)& 0xF) + 1
+	}
 })
